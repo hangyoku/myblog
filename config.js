4298030df2d144fe9c894bb13bb18cd599fb2984
hangzyoku <287831783@qq.com>
2018-01-10 23:51:51 +0800

    研究node中session的运用

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
  * Created by wdg01 on 2018/1/10.
  */
 const DB = {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "blog"
+    host: "localhost", // 地址
+    port: 3306, // 端口号
+    user: "root", // 用户
+    password: "", // 密码
+    database: "blog" // 数据库名
 };
 module.exports = DB;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var router = express.Router();
+
 
 var mysql = require('./../database');
 
-/* GET home page. */
+
+var app = express();
+
+app.use(cookieParser());
+app.use(session({
+    secret: '1321321', //secret的值建议使用随机字符串
+    cookie: {maxAge: 60 * 1000 * 30} // 过期时间（毫秒）
+}));
+
+/* 首页路由 */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+/* 登陆页面路由 */
 router.get('/login', function (req, res, next) {
     res.render('login', {message: ''});
 });
@@ -15,22 +28,28 @@ router.get('/login', function (req, res, next) {
 // 登录页面
 router.post('/login', function (req, res, next) {
 
+    // 获取请求的用户名
     var name = req.body.name;
+    // 获取请求的密码
     var password = req.body.password;
-
-    var query = 'SELECT * FROM blog WHERE authorName=' + mysql.escape(name) + ' AND authorPassword=' + mysql.escape(password);
+    // 查询数据库语句
+    var query = 'SELECT * FROM blog WHERE authorName=' + 
+                mysql.escape(name) + ' AND authorPassword=' + mysql.escape(password);
     mysql.query(query, function (err, rows, fields) {
         if (err){
             console.log(err);
             return;
         }
         var user = rows[0];
-        if (!user){
+        if (!user){ // 如果不存在 就提示用户错误
             res.render('login', {message: '用户名或者密码错误！'});
             return;
         }
+
         // req.session.userSign = true;
         // req.session.userId = user.authorID;
+
+        // 登陆成功 重定向页面
         res.redirect('/');
     })
 });
